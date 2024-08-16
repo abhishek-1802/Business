f58d6e32978ff07da7f09c312c0e9dda3730af24
abhishek-1802 <apuri_be20@thapar.edu>
2024-08-16 14:31:57 +0530

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Header.css'
+import {assets} from '../../assets/assets'
 
 export const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
+            <img src={assets.header_img} alt="" />
             <h2>Oder your favourite items here</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, a?</p>
             <button>View Menu</button>
