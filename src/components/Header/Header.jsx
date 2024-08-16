453159e6ef5c85f40b47d25f58c068dfe3fe1f29
abhishek-1802 <apuri_be20@thapar.edu>
2024-08-16 15:16:10 +0530

    third  commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <img src={assets.header_img} alt="" />
+            {/* <img src={assets.header_img} alt="Header Image" />   */}
             <h2>Oder your favourite items here</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, a?</p>
             <button>View Menu</button>
