70abd4f00bb51f64baac52b17ea3d916872d30f9
abhishek-1802 <apuri_be20@thapar.edu>
2024-08-16 13:30:08 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Oder your favourite items here</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus, a?</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
